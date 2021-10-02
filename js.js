82fe7e6d4a1f3a2c5b9c6a4a93a4e3c25a25be46
Elmira Naghibi <elmiranaghibi@gmail.com>
2021-10-02 11:33:17 +0330

    fea :  create clock with clock desktop with simple way

--- js.js
+++ js.js
@@ -1,113 +1,38 @@
 var nodeHtml = {
-    hour: document.getElementById("hour"),
-    min: document.getElementById("min"),
-    second: document.getElementById("second"),
-    inputHour: document.getElementById("inputHour"),
-    inputMin: document.getElementById("inputMin"),
-    inputSecond: document.getElementById("inputSecond"),
-    error: document.getElementById("error"),
-    btnClock: document.getElementById("btnClock")
-}
-var state = {
-    hour: "",
-    min: "",
-    second: "",
-    setingInt:""
-}
-
-function combineFunc() {
-    setClock()
-    displayClock()
-}
-
-function displayClock() {
-    handleHour()
-    handleMin()
-    handleSecond()
-}
-
-function handleError(item , maxItem , text){
-    if (!item ) return emptyBox(text),true;
-    if (item > maxItem || item < 0) return validateNumber(text),true;
-    return false
-}
-
-function validateNumber(text) {
-    nodeHtml.error.innerHTML += text + " " + "را درست وارد نمایید /"
-}
-
-function emptyBox(text) {
-    nodeHtml.error.innerHTML += text + " " + "را وارد کنید /"
-}
-
-function handleHour() {
-    var hourActive = state.hour > 12 ? state.hour - 12 : state.hour;
-    handleTransform(nodeHtml.hour, 30 * hourActive)
-}
-
-function handleMin() {
-    handleTransform(nodeHtml.min, 6 * state.min)
-}
-
-function handleSecond() {
-    handleTransform(nodeHtml.second, 6 * state.second)
-}
-
-function handleTransform(tag, deg) {
-    tag.style.transform = "rotate(" + deg + "deg)"
-}
-
-function init() {
-    SetEvents()
-}
-
-function removingError() {
-    nodeHtml.error.innerHTML = ""
-}
-
-function setClock() {
-    state.second++;
-    if (state.second == 60) {
-        state.second = 0;
-        state.min++;
-    };
-    if (state.min == 60) {
-        state.min = 0;
-        state.hour++;
-    }
-    console.log("second", state.second, "min", state.min, "hour", state.hour);
+    hour : document.getElementById("hour"),
+    min : document.getElementById("min"),
+    second : document.getElementById("second")
 }
 
 
-function SetEvents() {
-    nodeHtml.btnClock.addEventListener("click", setState)
-    nodeHtml.inputHour.addEventListener("focus", removingError)
-    nodeHtml.inputMin.addEventListener("focus", removingError)
-    nodeHtml.inputSecond.addEventListener("focus", removingError)
+function handleHour(hour){
+    var hourActive = hour > 12 ? hour - 12 : hour;
+    nodeHtml.hour.style.transform ="rotate("+30*hourActive+"deg)";
 
 }
 
-function setState(){
-    removingError()
-    clearInterval(state.setingInt);
-    nodeHtml.btnClock.disabled = true;
-    state.second = parseInt(nodeHtml.inputSecond.value);
-    state.min = parseInt(nodeHtml.inputMin.value);
-    state.hour = parseInt(nodeHtml.inputHour.value);
-    setFuncs(state.hour, state.min, state.second)
-
+function handleMin(min){
+    nodeHtml.min.style.transform ="rotate("+6*min+"deg)" 
 }
 
-function setFuncs(hour, min, second) {
-    var errorHour = handleError(hour , 23 ,"ساعت");
-    var errorMin = handleError(min ,59 ,"دقیقه");
-    var errorSecond = handleError(second, 59 ,"ثانیه");
-
-    nodeHtml.btnClock.disabled = false;
-
-    if (!errorHour && !errorMin && !errorSecond) {
-        state.setingInt= setInterval(combineFunc, 1000);
-    }
+function handleSecond(second){
+    nodeHtml.second.style.transform ="rotate("+6*second+"deg)"
 }
 
-init();
+
+function getClock() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var second = date.getSeconds();
+    handleHour(hour)
+    handleMin(min)
+    handleSecond(second)
+}
+
+
+function init(){
+    setInterval(getClock, 1000)
+}
+
+init()
